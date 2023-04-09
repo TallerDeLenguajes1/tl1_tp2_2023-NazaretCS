658d00e450dbf7dd2525b9a3c81e08e7c4eab011
NazaretCS <nazaretcampos27@gmail.com>
2023-04-09 18:16:27 -0300

    cargado de la solucion del tp2_punto2_d

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -8,7 +8,7 @@
 int main(){
 
     int i;
-    double vt[N]; 
+    double vt[N], *punt; 
     srand(time(NULL));
     
     for(i = 0;i<N; i++){
@@ -18,12 +18,17 @@ int main(){
         
     }
 
-    for(i = 0;i<N; i++){
-        
-        vt[i] = 1 + rand() % 100;
-        printf( "\n%f \n",  vt[i] );
-        
+    punt = vt;
+
+    printf("\nPrint de separacion: \n");
+
+    for (int j = 0; j < N; j++)
+    {
+        *(punt+j) = 1 + rand() % 100;
+        printf( "\n%f \n",  *(punt+j) );
+        punt++;
     }
+    
 
     return 0;
 }
